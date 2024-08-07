76f8dde62160d59def06c3549c68ce89b1b9ed01
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-08-07 14:37:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,9 @@
- /* main.tf
 provider "aws" {
  region = "ap-south-1"
 }
 
-resource "aws_instance" "exampleTFE" {
+resource "aws_instance" "exampleTFEharshit" {
   ami           = "ami-04b70fa74e45c3917"
   instance_type = "t2.micro"
-} */
-variable "Color" {
-   default = "white" 
-}
-resource "null_resource" "null1" {
-provisioner "local-exec" {
-    command = "echo $Color"
-  }
 }
 
