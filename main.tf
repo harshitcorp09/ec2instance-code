b7b299906e7e11cfef10d1351322f186dfef1afc
harshitcorp09 <162092528+harshitcorp09@users.noreply.github.com>
2024-11-25 19:55:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 provider "aws" {
- region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "exampleTFEharshit" {
   ami           = "ami-0ad21ae1d0696ad58"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleTFEHarshitInstance"
+  }
 }
 
